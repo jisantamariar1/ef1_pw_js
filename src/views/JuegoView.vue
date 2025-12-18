0c2098d1f7b5c12e270583db8ca090f6c9ca476d
jisantamariar1 <jisantamariar1@uce.edu.ec>
2025-12-17 19:16:11 -0500

    avance logica de ganar

--- src/views/JuegoView.vue
+++ src/views/JuegoView.vue
@@ -17,7 +17,11 @@
         <button v-on:click="aumentarContador()">Jugar</button>
     </div>
     </div>
-    <div class="gano"></div>
+    <div v-if="gano" class="gano">
+        <h1>Puntaje: {{ puntaje }}</h1>
+        <h1>Felicitaciones has ganado un premio de $10.000,00</h1>
+        <button v-on:click="reiniciarJuego()">Nuevo Juego</button>
+    </div>
     <div v-if="perdio" class="perdio">
         <h1>Haz utilizado tus 5 intentos</h1>
         <h1>El juego ha termindo, intentalo nuevamente</h1>
@@ -67,12 +71,17 @@ export default {
             this.puedeJugar=false;
             this.perdio=true;
         },
+        ganoJuego(){
+            this.puedeJugar=false;
+            this.gano=true;
+        },
         reiniciarJuego(){
             this.contadorV=0;
             this.puntaje=0;
             this.vectorResultado=[];
             this.puedeJugar=true;
             this.perdio=false;
+            this.gano=false;
         }
 
 
@@ -82,7 +91,12 @@ export default {
             if(newValue===5){
                 this.perdioJuego();
             }
-        }
+        },
+        puntaje(newValue, oldValue){
+            if(newValue>=10){
+                this.ganoJuego();
+            }
+        },
     }
 
 
@@ -118,5 +132,10 @@ button{
     text-align: center;
     margin-top: 200px;
     color:red;
+}
+.gano{
+    text-align: center;
+    margin-top: 200px;
+    color:rgb(11, 89, 235);
 }
 </style>
